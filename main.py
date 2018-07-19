bf0946352417b65052419d147a8b638350cfcda9
YTPSourceCode <41443083+YTPSourceCode@users.noreply.github.com>
2018-07-19 18:26:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import serial
+
+bms = serial.Serial('/dev/tty.usbserial',38400)
+
+while 1:
+    row_data = bms.readline()
+    clear_data = row_data.decode("utf-8")
+    try:
+        opcode = int(clear_data[6:8])
+    except:
+        continue
+    if opcode <10:
+        continue
+    if opcode == 26:
+        try:
+            minvolt = int(clear_data[13:17])
+            maxvolt = int(clear_data[18:22])
+            isi = float(clear_data[23:26])/10
+        except:
+            continue
+        if minvolt<3300:
+            continue
+        if maxvolt<3300:
+            continue
+        if isi>100:
+            continue
+        print('Max. Volt = {} \nMin. Volt = {}\nIsi = {}'.format(maxvolt,minvolt,isi))
+        print('      ')
+    if opcode == 24:
+        try:
+            current = int(clear_data[13:19])*10**-2
+            status = int(clear_data[20:21])
+        except:
+            continue
+        print('Current = {}'.format(current))
