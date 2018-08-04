d395cccd35c85bf0e3c59d8edda26b1cb6920b2f
YTPSourceCode <41443083+YTPSourceCode@users.noreply.github.com>
2018-08-04 20:58:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import serial
 
 bms = serial.Serial('/dev/tty.usbserial',38400)
-
 while 1:
+    print(' ')
     row_data = bms.readline()
     clear_data = row_data.decode("utf-8")
     try:
@@ -11,26 +11,20 @@ while 1:
         continue
     if opcode <10:
         continue
-    if opcode == 26:
+    elif opcode == 26:
         try:
             minvolt = int(clear_data[13:17])/1000
             maxvolt = int(clear_data[18:22])/1000
             isi = float(clear_data[23:26])/10
-            battery = float(((minvolt+maxvolt)/2)*20)*1.01
+            battery_voltage = float(((minvolt+maxvolt)/2)*20)
+            battery = (battery_voltage - 56)*3.9
         except:
             continue
-        if minvolt<3300:
-            continue
-        if maxvolt<3300:
-            continue
-        if isi>100:
-            continue
-        print('Max. Volt = {} \nMin. Volt = {}\nIsi = {}'.format(maxvolt,minvolt,isi))
-        print('      ')
-    if opcode == 24:
+        print('Max. Volt = {} \nMin. Volt = {}\nIsi = {}\nBatteryControl = {}\nBattery = {}'.format(maxvolt,minvolt,isi,battery_control,battery))
+    elif opcode == 24:
         try:
             current = int(clear_data[13:19])*10**-2
             status = int(clear_data[20:21])
         except:
             continue
-        print('Current = {}'.format(current))
+        print('current: {}\nBattery Status = {}'.format(current,status))
